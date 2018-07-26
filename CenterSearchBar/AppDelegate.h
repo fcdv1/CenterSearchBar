925f3a0410c0ee5e13f5a4246bccc2c04255230d
cia <245185601@qq.com>
2018-07-26 15:54:35 +0800

    Initial Commit

---
+++ CenterSearchBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CenterSearchBar
+//
+//  Created by cia on 2018/7/26.
+//  Copyright © 2018年 ZFH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
